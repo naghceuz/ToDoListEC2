59fbfa3bfd3683f6e52c87ea945cb11f16adbecf
naghceuz <guanchenzhang@gmail.com>
2015-03-09 01:34:05 -0400

    do some change

---
+++ log.md
@@ -0,0 +1,10 @@
+Feb 18th - Feb 19th 
+
+1.根据需求重新构架
+
+大概是: Todo schema 在设计的时候，增加一个author 结构
+这样在每次增加Todo 的时候，
+
+
+
+首先
--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,10 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'ToDo List with Mongoose and Express', h1: 'ToDo List'});
-};
+// exports.index = function(req, res){
+//   res.render('index', { title: 'ToDo List with Mongoose and Express', h1: 'ToDo List'});
+// };
 
-exports.remove = function(req, res){
-  res.render('remove', {title: 'ToDo List with Mongoose and Express', h1: 'Remove an item!'})
-};
+// exports.remove = function(req, res){
+//   res.render('remove', {title: 'ToDo List with Mongoose and Express', h1: 'Remove an item!'})
+// };
